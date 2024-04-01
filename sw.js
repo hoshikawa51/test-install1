1cbe83ca4952efec4b3424e4659a9af1b9a05b7b
hoshikawa51 <kouichi_hoshikawa@i-set.co.jp>
2024-04-01 16:44:29 +0900

    お試し１１

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-var CACHE_NAME = 'sample-v5';
-var urlsToCache = ['/index.html', '/menu.html'];
+var CACHE_NAME = 'sample-v6';
+var urlsToCache = ['/index.html', '/menu.html', '/app.js'];
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
